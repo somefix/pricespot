57a92f2d32fee3e4b09a392ed9e5f1aff23de9ab
Ilya grigoriev <grigoriev.tver@gmail.com>
2024-10-14 10:01:06 +0300

    add ya.metrika

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,38 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'PriceSpot',
+      htmlAttrs: {
+        lang: 'ru'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Мониторинг и анализ цен' },
+        // { name: 'yandex-verification', content: 'b15fe8bd630f2147' },
+      ],
+    },
+    baseURL: process.env?.BASE_URL ?? '/',
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  modules: [
+    '@artmizu/yandex-metrika-nuxt',
+  ],
+  yandexMetrika: {
+    // @ts-ignore
+    id: process.env.YANDEX_METRIKA_ID,
+    initParams: {
+      webvisor:true,
+      clickmap:true,
+      trackLinks:true,
+      accurateTrackBounce:true,
     },
   },
 })
